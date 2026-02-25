eb3ffcceaf3452887f0b1b7d339f1e8873f94dc6
Bruce C. Miller <bm3719@gmail.com>
2026-02-25 18:39:39 -0500

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clj-uke "0.1.0-SNAPSHOT"
+  :description "Ukulele note recognition"
+  :url "http://github.com/bm3719/clj-uke"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :repl-options {:init-ns clj-uke.core})
---
+++ test/clj_uke/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-uke.core-test
+  (:require [clojure.test :refer :all]
+            [clj-uke.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
