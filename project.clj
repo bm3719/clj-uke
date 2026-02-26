29d9df20d70e93ee6ca0d0a3d8ced4c7523e5103
Bruce C. Miller <bm3719@gmail.com>
2026-02-25 19:22:38 -0500

    Get super basic version working

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://github.com/bm3719/clj-uke"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[metosin/malli "0.20.0"]
+                 [org.clojure/clojure "1.12.4"]]
   :repl-options {:init-ns clj-uke.core})
--- src/clj_uke/core.clj
+++ src/clj_uke/core.clj
@@ -1,6 +1,46 @@
-(ns clj-uke.core)
+(ns clj-uke.core
+  (:require [malli.generator :as mg]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; TODO: Add more notes here later
+(def notes
+  (map vector
+       [:c :d :e :f :g :a :b :c :d]
+       (reverse (range 11))))
+
+(def staff-template
+  "ASCII staff rows (top to bottom)."
+  (->> (cycle    ["-----------"
+                  "           "])
+       (take 10)
+       vec
+       (#(conj %1 "     -     "))))
+
+(def seed (atom 1807))
+
+(defn get-next-seed []
+  (swap! seed inc))
+
+(defn rand-note []
+  (mg/generate (vec (cons :enum notes)) {:seed (get-next-seed)}))
+
+(defn replace-char [s index new-char]
+  (str (subs s 0 index) new-char (subs s (inc index))))
+
+(defn insert-note [note]
+  (for [line (range (count staff-template))]
+    (if (= line (second note))
+      (replace-char (get staff-template line) 5 "O")
+      (get staff-template line))))
+
+(defn gen-note []
+  (let [note (rand-note)
+        msur (insert-note note)]
+    (doseq [line msur]
+      (println line))))
+
+(defn -main [& args]
+  (future
+    (while true
+      (println "")
+      (gen-note) ; Generate note.
+      (Thread/sleep 8000))))
